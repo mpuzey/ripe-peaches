9997a2eeded91b31b4ccd0856dae6d2b7db76f5d
matthew.puzey <matthew.puzey@nhs.net>
2018-06-23 15:36:02 +0100

    fix store reviews test

--- unit_tests/collector/test_service.py
+++ unit_tests/collector/test_service.py
@@ -9,20 +9,22 @@ class TestService(unittest.TestCase):
     @patch('collector.service.Scraper')
     def test__service__CollectorService__start__WillCallScraperCollect__WhenCalled(self, mock_scraper, _):
 
+        scraper_instance = mock_scraper.return_value
+
         CollectorService.start()
 
-        scraper_instance = mock_scraper.return_value
         assert scraper_instance.collect.called
 
     @patch('collector.service.ReviewStore')
     @patch('collector.service.Scraper')
     def test__service__CollectorService__start__WillStoreReviews__WhenCalled(self, mock_scraper, mock_store):
 
-        CollectorService.start()
         scraper_instance = mock_scraper.return_value
         store_instance = mock_store.return_value
-
+        
         test_data = {'some_data': 'scoob'}
         scraper_instance.collect.return_value = test_data
+
+        CollectorService.start()
 
         store_instance.store_reviews.assert_called_with(test_data)
