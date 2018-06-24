bf52e4438c594402b7a037aeac8cd81ae91897c0
matthew.puzey <matthew.puzey@nhs.net>
2018-06-24 20:25:36 +0100

    add test for missing full reviews

--- unit_tests/collector/sources/meta_critic_html.py
+++ unit_tests/collector/sources/meta_critic_html.py
@@ -40,6 +40,45 @@ def get_reviews_html():
 """
 
 
+def get_review_without_full_review():
+    return """
+<li class="review critic_review">
+    <div class="review_wrap">
+        <div class="review_content">
+            <div class="review_section">
+                <div class="review_product_scores">
+                    <ul class="scores">
+                        <li class="review_product_score brief_metascore">
+                            <span class="metascore_w small release positive">84</span>
+                            <span class="label">Metascore</span>
+                        </li>
+                        <li class="review_product_score brief_critscore">
+                            <span class="metascore_w small release positive indiv">80</span>
+                            <span class="label">
+                                <span class="name_only">Critic Score</span>
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="review_product">
+                    <a href="/music/our-raw-heart/yob">Our Raw Heart</a>
+                </div>
+                <div class="review_body">
+                    Our Raw Heart is a crushing and stirring doom metal affair, a cathartic album created after guitarist and vocalist Mike Scheidt suffered a severe episode of diverticulitis early last year. It shines with a rare beauty. The music ebbs and flows from ballad-like meditations reminiscent of Earth to the caustic sludge of Yob’s early records.
+                </div>
+            </div>
+            <div class="review_section review_actions">
+                <ul class="review_actions">
+                    <li class="review_action publication_title">The Quietus</li>
+                    <div class="review_action post_date">Posted Jun 20, 2018</div>
+                </ul>
+            </div>
+        </div>
+    </div>
+</li>
+"""
+
+
 def get_first_review_html():
     return """
 <li class="review critic_review first_review">
--- unit_tests/collector/sources/test_metacritic.py
+++ unit_tests/collector/sources/test_metacritic.py
@@ -1,7 +1,8 @@
 from mock import patch, MagicMock
 import unittest
 from collector.sources import metacritic
-from unit_tests.collector.sources.meta_critic_html import get_reviews_html, get_first_review_html
+from unit_tests.collector.sources.meta_critic_html import get_reviews_html, get_first_review_html, \
+get_review_without_full_review
 
 
 class TestMetacritic(unittest.TestCase):
@@ -41,6 +42,27 @@ class TestMetacritic(unittest.TestCase):
                 'release_name': 'DAYTONA',
                 'score': '80',
                 'link': 'http://thequietus.com/articles/24834-pusha-t-daytona-album-review'
+            }
+        ]
+
+        actual_reviews = metacritic.get_reviews('the-quietus')
+
+        self.assertEqual(actual_reviews, expected_reviews)
+
+    @patch('collector.sources.metacritic.requests')
+    def test__metacritic__get_reviews__WillIncludeFirstReview_WhenMetacriticHTMLIncludesFirstReviewHTMLClass(self, mock_requests):
+
+        response = MagicMock()
+        response.text = get_review_without_full_review()
+        mock_requests.get.return_value = response
+        expected_reviews = [
+            {
+                'artist': 'Yob',
+                'date': 'Posted Jun 20, 2018',
+                'publication_name': 'The Quietus',
+                'release_name': 'Our Raw Heart',
+                'score': '80',
+                'link': None
             }
         ]
 
