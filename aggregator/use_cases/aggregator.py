3f833518eea1544c33d6919a3fac455d730911a0
Alistair Webster <alistair.webster@nhs.net>
2018-07-18 09:22:20 +0100

    refactor aggregator

--- aggregator/use_cases/aggregator.py
+++ aggregator/use_cases/aggregator.py
@@ -10,28 +10,42 @@ class Aggregator(DataWorker):
 
         scores = {}
 
-        for key, value in artists.items():
-            artist_name = value.get('name')
-            release_ids = value.get('releases')
-            if release_ids:
-                for release_id in release_ids:
-                    release = releases.get(release_id)
-                    release_name = release.get('name')
-                    review_ids = release.get('reviews')
-                    if review_ids:
-                        release_scores = [int(reviews.get(review_id).get('score')) for review_id in review_ids]
+        for artist_id, artist in artists.items():
 
-                        score_id = calculate_hash(artist_name + release_name)
-                        aggregated_score = sum(release_scores) / len(release_scores)
+            score = self.__aggregate(artist, releases, reviews)
+            scores.update(score)
 
-                        scores[score_id] = {
-                            'id': score_id,
-                            'release_id': release_id,
-                            'release_name': release_name,
-                            'artist_id': key,
-                            'artist_name': artist_name,
-                            'score': aggregated_score
-
-                        }
-        '''[e**2 for e in a_list if type(e) == types.IntType]'''
         return scores
+
+    def __aggregate(self, artist, releases, reviews):
+
+        artist_id = artist.get('id')
+        artist_name = artist.get('name')
+
+        score = {}
+        for release_id in artist.get('releases'):
+            release = releases.get(release_id)
+            release_name = release.get('name')
+
+            aggregate_score = self.__aggregate_release_score(release, reviews)
+
+            score_id = calculate_hash(artist_name + release_name)
+
+            score[score_id] = {
+                'id': score_id,
+                'release_id': release_id,
+                'release_name': release_name,
+                'artist_id': artist_id,
+                'artist_name': artist_name,
+                'score': aggregate_score
+            }
+
+        return score
+
+    def __aggregate_release_score(self, release, reviews):
+
+        review_ids = release.get('reviews')
+        release_scores = [int(reviews.get(review_id).get('score')) for review_id in review_ids]
+        aggregated_score = sum(release_scores) / len(release_scores)
+
+        return aggregated_score
