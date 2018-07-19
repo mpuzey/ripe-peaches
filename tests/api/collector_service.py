7b532b68f8abfaaa2011567f65423ff80326b8d7
matthew.puzey <matthew.puzey@nhs.net>
2018-07-19 19:31:50 +0100

    :peach: use test api in a service test remove brittle test with call assertion

---
+++ tests/api/collector_service.py
@@ -0,0 +1,24 @@
+"""
+The purpose of this module is to provide an API to test the collector service that is easy to use
+in discrete tests and allows us to keep changes to tests to a minimum when functionality changes.
+Instead where there are major refactors only the test API need change , where there are minor
+refactors the tests and API need not. We should only need to add and change tests when there is new
+functionality, a bug to fix or a missing scenario to cover.
+"""
+
+from mock import patch
+
+from collector.service import CollectorService
+
+
+def service_starts(collector_instance, collected_data):
+
+    with patch('collector.service.ArtistStore') as mock_artist_store, \
+         patch('collector.service.ReleaseStore') as mock_release_store, \
+         patch('collector.service.aoty') as mock_aoty, \
+         patch('collector.service.metacritic') as mock_metacritic:
+
+        collector_instance.reviews = collected_data
+
+        collector_service = CollectorService(collector_instance)
+        collector_service.start()
--- tests/service_tests/test_collector_service.py
+++ tests/service_tests/test_collector_service.py
@@ -1,49 +1,19 @@
-from mock import MagicMock, call, patch
 import unittest
 from collector.controllers.review_scraper import ReviewScraper
-from collector.service import CollectorService
 from tests.service_tests.test_data import get_artists_sample, get_review_list
-from constants import METACRITIC_PUBLICATIONS_SAMPLE, AOTY_PUBLICATIONS_SAMPLE
+from tests.api.collector_service import service_starts
 
 
 class TestService(unittest.TestCase):
 
-    @patch('collector.service.FileAdapter')
-    @patch('collector.service.aoty')
-    @patch('collector.service.metacritic')
-    def test__service__CollectorService__start__WillCallCollectorCollect__WhenCalled(self, mock_metacritic, mock_aoty, _):
-
-        mock_collector = MagicMock()
-        collector_instance = mock_collector.return_value
-        collector_service = CollectorService(collector_instance)
-        collector_service.start()
-
-        calls = [call(METACRITIC_PUBLICATIONS_SAMPLE, mock_metacritic),
-                 call(AOTY_PUBLICATIONS_SAMPLE, mock_aoty)]
-
-        collector_instance.collect.assert_has_calls(calls)
-
-    @patch('collector.service.ReleaseStore')
-    @patch('collector.service.ArtistStore')
-    @patch('collector.service.aoty')
-    @patch('collector.service.metacritic')
-    def test__service__CollectorService__start__WillAddArtistsCatalogToCollector__WhenCalled(self, mock_metacritic, mock_aoty, mock_artist_store, mock_release_store):
+    def test__service__CollectorService__start__WillAddArtistsCatalogToCollector__WhenCalled(self):
 
         collector_instance = ReviewScraper()
-        collector_instance.reviews = get_review_list()
-
         expected_artists = get_artists_sample()
 
-        collector_service = CollectorService(collector_instance)
-        collector_service.start()
+        service_starts(collector_instance, get_review_list())
 
         self.assertEqual(collector_instance.artists, expected_artists)
 
-    @patch('collector.service.FileAdapter')
-    @patch('collector.service.aoty')
-    @patch('collector.service.metacritic')
-    def test__service__CollectorService__store_collection__WillStoreArtists__WhenCalled(self,
-                                                                                        mock_metacritic,
-                                                                                        mock_aoty,
-                                                                                        _):
+    def test__service__CollectorService__store_collection__WillStoreArtists__WhenCalled(self):
         pass
