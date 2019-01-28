c6d0bee34e0db920e75feb07a84601b87f61b627
Matt Puzey <matthew.puzey@skybettingandgaming.com>
2019-01-28 20:07:44 +0000

    :peach: refactor new release search for io at the top

--- src/collector/web/spotify.py
+++ src/collector/web/spotify.py
@@ -4,51 +4,49 @@ import base64
 import requests
 import constants
 
+
 def get_releases():
 
-    access_token = auth()
+    details = load_credentials()
+    authorization_response = requests.post('https://accounts.spotify.com/api/token',
+                                           data={'grant_type': 'client_credentials'},
+                                           headers={'Authorization': details})
 
-    raw_releases = get_recent_releases(access_token)
+    access_token = json.loads(authorization_response.text).get('access_token')
+    next_page = constants.SPOTIFY_NEW_RELEASE_SEARCH
+    raw_releases = []
+
+    while True:
+        response = requests.get(next_page, headers={'Authorization': 'Bearer %s' % access_token}).json()
+        new_items, next_page = parse_response(response)
+
+        raw_releases.extend(new_items)
+
+        if not next_page:
+            break
 
     return parse_releases(raw_releases)
 
 
-def auth():
+def load_credentials():
+
     client_id = os.environ.get('SPOTIFY_CLIENT_ID')
     client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET')
     client_details = '%s:%s' % (client_id, client_secret)
     encoded_client_details = base64.b64encode(client_details.encode('utf-8')).decode('utf-8')
     details = 'Basic %s' % encoded_client_details
 
-    authorization_response = requests.post('https://accounts.spotify.com/api/token',
-                                           data={'grant_type': 'client_credentials'},
-                                           headers={'Authorization': details})
-
-    return json.loads(authorization_response.text).get('access_token')
+    return details
 
 
-def get_recent_releases(access_token):
+def parse_response(response):
 
-    url = constants.SPOTIFY_NEW_RELEASE_SEARCH
-    items = []
+    albums = response.get('albums')
 
-    while True:
-        response = requests.get(url, headers={'Authorization': 'Bearer %s' % access_token}).json()
+    if not albums:
+        return None
 
-        albums = response.get('albums')
-
-        if not albums:
-            return None
-
-        new_items = albums.get('items')
-        url = albums.get('next')
-
-        items.extend(new_items)
-
-        if not url:
-            break
-
-    return items
+    return albums.get('items'), albums.get('next')
 
 
 def parse_releases(raw_releases):
