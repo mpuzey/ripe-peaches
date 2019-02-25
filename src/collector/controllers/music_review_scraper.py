09f9574a3b3118a3c6d98bf86d727c49c948349a
Alistair Webster <alistair.webster@skybettingandgaming.com>
2019-02-25 13:13:32 +0000

    :peach: initial stab at refactoring parsing logic for music reviews and releases

--- src/collector/controllers/music_review_scraper.py
+++ src/collector/controllers/music_review_scraper.py
@@ -1,13 +1,11 @@
-from src.collector.entities.collector import Collector
-
-from src.common.crypto import calculate_hash
+from src.collector.use_cases.collector import Collector
 
 
 class MusicReviewScraper(Collector):
 
-    def __init__(self):
+    def __init__(self, parser):
         self.raw_reviews = []
-        self.artists = {}
+        self.parser = parser
 
     def collect(self, source, **kwargs):
 
@@ -24,68 +22,5 @@ class MusicReviewScraper(Collector):
         print('finished scraping!')
 
     def parse(self):
-        for raw_review in self.raw_reviews:
 
-            artist_id = self._build_artist(raw_review)
-
-            release_id = self._build_release(artist_id, raw_review)
-
-            self._build_review(raw_review, artist_id, release_id)
-
-        return self.artists
-
-    def _build_artist(self, raw_review):
-
-        artist_name = raw_review.get('artist')
-        artist_id = calculate_hash(artist_name)
-
-        if not self.artists.get(artist_id):
-            self.artists[artist_id] = {
-                'id': artist_id,
-                'name': artist_name,
-                'releases': {}
-            }
-
-        return artist_id
-
-    def _build_release(self, artist_id, raw_review):
-
-        artist_name = raw_review.get('artist')
-        release_name = _format_release_name(raw_review.get('release_name'))
-
-        release_id = calculate_hash(artist_name + release_name)
-
-        existing_release = self.artists.get(artist_id).get('releases').get(release_id)
-        if not existing_release:
-            self.artists[artist_id]['releases'][release_id] = {
-                'id': calculate_hash(artist_name + release_name),
-                'name': release_name,
-                'reviews': {}
-            }
-
-        return release_id
-
-    def _build_review(self, raw_review, artist_id, release_id):
-
-        artist_name = raw_review.get('artist')
-        release_name = _format_release_name(raw_review.get('release_name'))
-        publication_name = raw_review.get('publication_name')
-
-        review = {
-            'score': raw_review.get('score'),
-            'publication_name': publication_name,
-            'date': raw_review.get('date'),
-            'link': raw_review.get('link')
-        }
-
-        review_id = calculate_hash(artist_name + release_name + publication_name)
-        review['id'] = review_id
-
-        self.artists[artist_id]['releases'][release_id]['reviews'][review_id] = review
-
-
-def _format_release_name(name):
-
-    formatted_name = name.replace('and', '&').title()
-
-    return formatted_name
+        return self.parser.parse(self.raw_reviews)
