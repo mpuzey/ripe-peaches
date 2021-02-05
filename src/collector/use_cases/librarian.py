9664bf059a8a720f4eadc3436aeaad9a264b1a46
MattPuzey <mpuzey1@outlook.com>
2021-02-05 17:52:36 +0000

    :peach: wip refactor to give catalogers a catalog to store the artists against and consolidating music cataloging back into one class

---
+++ src/collector/use_cases/librarian.py
@@ -0,0 +1,22 @@
+from typing import List, Dict
+from src.collector.entities.release import Release
+from src.collector.entities.review import Review
+from src.collector.entities.artist import Artist
+from src.collector.entities.publication_review import PublicationReview
+from src.collector.entities.publication_release import PublicationRelease
+from abc import ABC, abstractmethod
+
+
+class Librarian(ABC):
+
+    @abstractmethod
+    def add_reviews(self, publication_reviews: List[PublicationReview]) -> Dict[str, Artist]:
+        pass
+
+    @abstractmethod
+    def add_releases(self, publication_releases: List[PublicationRelease]) -> Dict[str, Artist]:
+        pass
+
+    @abstractmethod
+    def format_release_name(self, name: str) -> str:
+        pass
---
+++ tests/unit_tests/collector/test_music_release_collector.py
@@ -0,0 +1,7 @@
+import unittest
+
+
+class TestMusicReleaseCollector(unittest.TestCase):
+
+    def test__MusicReleaseCollector__collect__WillBuildUpAListOfCuratedReviewsOnInstance__WhenCalledTwice(self):
+        pass
