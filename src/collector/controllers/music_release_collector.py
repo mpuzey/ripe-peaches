aa2e41132ff9e5c97fe3b0a03cd5ffed072d5ee4
MattPuzey <mpuzey1@outlook.com>
2021-02-09 15:35:34 +0000

    :peach: remove outdated todo

--- src/collector/controllers/music_release_collector.py
+++ src/collector/controllers/music_release_collector.py
@@ -18,8 +18,6 @@ class MusicReleaseCollector(Collector):
         self.external_releases.extend(releases)
 
     def catalog(self):
-        # TODO: use the music cataloger here
-
         # TODO: using the cataloging use case at the controller is a code smell we should keep these
         #  pieces of functionality separate, the cataloger should maybe hold a collector instead
         return self.cataloger.add_releases(self.external_releases)
