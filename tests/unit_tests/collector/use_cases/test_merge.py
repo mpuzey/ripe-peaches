c723eff5d8f3c472b1c5dd1f2febfd7cabfaf79c
MattPuzey <mpuzey1@outlook.com>
2021-08-20 14:43:04 +0100

    :peach: make ids in merge tests easier to read

--- tests/unit_tests/collector/use_cases/test_merge.py
+++ tests/unit_tests/collector/use_cases/test_merge.py
@@ -10,62 +10,56 @@ class TestMergeArtistDicts(unittest.TestCase):
 
     def test__merge_artist_dicts__WillAddArtistReleaseAndReviewToArchive_WhenArtistNotSeenBefore(self):
         archived_artists = {
-            '8ba02ddc3c2b1c1887a7788f490e940f589bd600eed30734eefa29ec5c2b1440': Artist(
-                id='8ba02ddc3c2b1c1887a7788f490e940f589bd600eed30734eefa29ec5c2b1440',
+            'artist_id_123': Artist(
+                id='artist_id_123',
                 name='Deafheaven',
                 releases=[]
             )
         }
 
         recently_review_artists = {
-            'd92a4901abc2f02dfd347e0793ca3f1c223cbff321d30cdef2679ed13b1c58ae': Artist(
-                id='d92a4901abc2f02dfd347e0793ca3f1c223cbff321d30cdef2679ed13b1c58ae',
+            'artist_id_456': Artist(
+                id='artist_id_456',
                 name='YOB',
                 releases=[
                     Release(
-                        id='137801a1514b811c49b2d27183e4b6fd8b7371d76cc4ff177c0a70d9700e196c',
+                        id='release_id_123',
                         name='Clearing The Path',
                         reviews=[
                             Review(
-                                id='af9896e8834f324e2ad0aa281985cc9b13534ec3b2b81414f05df508ef0f2f0b',
+                                id='review_id_123',
                                 publication_name='pitchfork',
                                 score=80,
                                 date='',
                                 link='')
-                        ],
-                        date=None,
-                        type=None,
-                        total_tracks=None,
-                        spotify_url=None),
+                        ]
+                    ),
                 ]
             )
         }
 
         expected_artists = {
-            '8ba02ddc3c2b1c1887a7788f490e940f589bd600eed30734eefa29ec5c2b1440': Artist(
-                id='8ba02ddc3c2b1c1887a7788f490e940f589bd600eed30734eefa29ec5c2b1440',
+            'artist_id_123': Artist(
+                id='artist_id_123',
                 name='Deafheaven',
                 releases=[]
             ),
-            'd92a4901abc2f02dfd347e0793ca3f1c223cbff321d30cdef2679ed13b1c58ae': Artist(
-                id='d92a4901abc2f02dfd347e0793ca3f1c223cbff321d30cdef2679ed13b1c58ae',
+            'artist_id_456': Artist(
+                id='artist_id_456',
                 name='YOB',
                 releases=[
                     Release(
-                        id='137801a1514b811c49b2d27183e4b6fd8b7371d76cc4ff177c0a70d9700e196c',
+                        id='release_id_123',
                         name='Clearing The Path',
                         reviews=[
                             Review(
-                                id='af9896e8834f324e2ad0aa281985cc9b13534ec3b2b81414f05df508ef0f2f0b',
+                                id='review_id_123',
                                 publication_name='pitchfork',
                                 score=80,
                                 date='',
                                 link='')
-                        ],
-                        date=None,
-                        type=None,
-                        total_tracks=None,
-                        spotify_url=None),
+                        ]
+                    ),
                 ]
             )
         }
@@ -75,98 +69,167 @@ class TestMergeArtistDicts(unittest.TestCase):
         assert actual_artists == expected_artists
 
     def test__merge_artist_dicts__WillAddArtistMultipleReleasesAndReviewToArchive_WhenArtistNotSeenBefore(self):
-        pass
+        archived_artists = {
+            'artist_id_123': Artist(
+                id='artist_id_123',
+                name='Deafheaven',
+                releases=[]
+            )
+        }
+
+        recently_review_artists = {
+            'artist_id_456': Artist(
+                id='artist_id_456',
+                name='YOB',
+                releases=[
+                    Release(
+                        id='release_id_123',
+                        name='Clearing The Path',
+                        reviews=[
+                            Review(
+                                id='review_id_123',
+                                publication_name='pitchfork',
+                                score=80,
+                                date='',
+                                link='')
+                        ]
+                    ),
+                    Release(
+                        id='release_id_456',
+                        name='Atma',
+                        reviews=[
+                            Review(
+                                id='review_id_456',
+                                publication_name='pitchfork',
+                                score=80,
+                                date='',
+                                link='')
+                        ]
+                    )
+                ]
+            )
+        }
+
+        expected_artists = {
+            'artist_id_123': Artist(
+                id='artist_id_123',
+                name='Deafheaven',
+                releases=[]
+            ),
+            'artist_id_456': Artist(
+                id='artist_id_456',
+                name='YOB',
+                releases=[
+                    Release(
+                        id='release_id_123',
+                        name='Clearing The Path',
+                        reviews=[
+                            Review(
+                                id='review_id_123',
+                                publication_name='pitchfork',
+                                score=80,
+                                date='',
+                                link='')
+                        ]
+                    ),
+                    Release(
+                        id='release_id_456',
+                        name='Atma',
+                        reviews=[
+                            Review(
+                                id='review_id_456',
+                                publication_name='pitchfork',
+                                score=80,
+                                date='',
+                                link='')
+                        ]
+                    )
+                ]
+            )
+        }
+
+        actual_artists = merge_artist_dicts(archived_artists, recently_review_artists)
+
+        assert actual_artists == expected_artists
 
     def test__merge_artist_dicts__WillAddArtistReleaseAndMultipleReviewsToArchive_WhenArtistNotSeenBefore(self):
         pass
 
     def test__merge_artist_dicts__WillAddReleaseAndReviewToArchive_WhenReleaseNotSeenBefore(self):
         archived_artists = {
-            'd92a4901abc2f02dfd347e0793ca3f1c223cbff321d30cdef2679ed13b1c58ae': Artist(
-                id='d92a4901abc2f02dfd347e0793ca3f1c223cbff321d30cdef2679ed13b1c58ae',
+            'artist_id_123': Artist(
+                id='artist_id_123',
                 name='YOB',
                 releases=[
                     Release(
-                        id='137801a1514b811c49b2d27183e4b6fd8b7371d76cc4ff177c0a70d9700e196c',
+                        id='release_id_123',
                         name='Clearing The Path',
                         reviews=[
                             Review(
-                                id='af9896e8834f324e2ad0aa281985cc9b13534ec3b2b81414f05df508ef0f2f0b',
+                                id='review_id_123',
                                 publication_name='pitchfork',
                                 score=80,
                                 date='',
                                 link='')
                         ],
-                        date=None,
-                        type=None,
-                        total_tracks=None,
-                        spotify_url=None),
+                    ),
                 ]
             )
 
         }
 
         recently_review_artists = {
-            'd92a4901abc2f02dfd347e0793ca3f1c223cbff321d30cdef2679ed13b1c58ae': Artist(
-                id='d92a4901abc2f02dfd347e0793ca3f1c223cbff321d30cdef2679ed13b1c58ae',
+            'artist_id_123': Artist(
+                id='artist_id_123',
                 name='YOB',
                 releases=[
                     Release(
-                        id='1',
+                        id='release_id_456',
                         name='Atma',
                         reviews=[
                             Review(
-                                id=1,
+                                id='review_id_456',
                                 publication_name='pitchfork',
                                 score=85,
                                 date='',
                                 link=''
                             )
                         ],
-                    date=None,
-                    type=None,
-                    total_tracks=None,
-                    spotify_url=None)
+                    )
                 ]
             )
         }
 
         expected_artists = {
-            'd92a4901abc2f02dfd347e0793ca3f1c223cbff321d30cdef2679ed13b1c58ae': Artist(
-                id='d92a4901abc2f02dfd347e0793ca3f1c223cbff321d30cdef2679ed13b1c58ae',
+            'artist_id_123': Artist(
+                id='artist_id_123',
                 name='YOB',
                 releases=[
                     Release(
-                        id='137801a1514b811c49b2d27183e4b6fd8b7371d76cc4ff177c0a70d9700e196c',
+                        id='release_id_123',
                         name='Clearing The Path',
                         reviews=[
                             Review(
-                                id='af9896e8834f324e2ad0aa281985cc9b13534ec3b2b81414f05df508ef0f2f0b',
+                                id='review_id_123',
                                 publication_name='pitchfork',
                                 score=80,
                                 date='',
                                 link='')
                         ],
-                        date=None,
-                        type=None,
-                        total_tracks=None,
-                        spotify_url=None),
+                    ),
                     Release(
-                        id='1',
+                        id='release_id_456',
                         name='Atma',
                         reviews=[
                             Review(
-                                id=1,
+                                id='review_id_456',
                                 publication_name='pitchfork',
                                 score=85,
                                 date='',
                                 link=''
                             )
                         ],
-                        date=None,
-                        type=None,
-                        total_tracks=None,
-                        spotify_url=None)
+                    )
                 ]
             )
         }
@@ -177,82 +240,73 @@ class TestMergeArtistDicts(unittest.TestCase):
 
     def test__merge_artist_dicts__WillAddReviewToArchive_WhenReviewNotSeenBefore(self):
         archived_artists = {
-            'd92a4901abc2f02dfd347e0793ca3f1c223cbff321d30cdef2679ed13b1c58ae': Artist(
-                id='d92a4901abc2f02dfd347e0793ca3f1c223cbff321d30cdef2679ed13b1c58ae',
+            'artist_id_123': Artist(
+                id='artist_id_123',
                 name='YOB',
                 releases=[
                     Release(
-                        id='137801a1514b811c49b2d27183e4b6fd8b7371d76cc4ff177c0a70d9700e196c',
+                        id='release_id_123',
                         name='Clearing The Path',
                         reviews=[
                             Review(
-                                id='af9896e8834f324e2ad0aa281985cc9b13534ec3b2b81414f05df508ef0f2f0b',
+                                id='review_id_123',
                                 publication_name='pitchfork',
                                 score=80,
                                 date='',
                                 link='')
                         ],
-                        date=None,
-                        type=None,
-                        total_tracks=None,
-                        spotify_url=None),
+                    ),
                 ]
             )
 
         }
 
         recently_review_artists = {
-            'd92a4901abc2f02dfd347e0793ca3f1c223cbff321d30cdef2679ed13b1c58ae': Artist(
-                id='d92a4901abc2f02dfd347e0793ca3f1c223cbff321d30cdef2679ed13b1c58ae',
+            'artist_id_123': Artist(
+                id='artist_id_123',
                 name='YOB',
                 releases=[
                     Release(
-                        id='137801a1514b811c49b2d27183e4b6fd8b7371d76cc4ff177c0a70d9700e196c',
+                        id='release_id_123',
                         name='Clearing The Path',
                         reviews=[
                             Review(
-                                id=1,
+                                id='review_id_456',
                                 publication_name='the needledrop',
                                 score=70,
                                 date='',
                                 link=''
                             )
-                        ],
-                        date=None,
-                        type=None,
-                        total_tracks=None,
-                        spotify_url=None)
+                        ]
+                    )
                 ]
             )
         }
 
         expected_artists = {
-            'd92a4901abc2f02dfd347e0793ca3f1c223cbff321d30cdef2679ed13b1c58ae': Artist(
-                id='d92a4901abc2f02dfd347e0793ca3f1c223cbff321d30cdef2679ed13b1c58ae',
+            'artist_id_123': Artist(
+                id='artist_id_123',
                 name='YOB',
                 releases=[
                     Release(
-                        id='137801a1514b811c49b2d27183e4b6fd8b7371d76cc4ff177c0a70d9700e196c',
+                        id='release_id_123',
                         name='Clearing The Path',
                         reviews=[
                             Review(
-                                id='af9896e8834f324e2ad0aa281985cc9b13534ec3b2b81414f05df508ef0f2f0b',
+                                id='review_id_123',
                                 publication_name='pitchfork',
                                 score=80,
                                 date='',
                                 link=''),
                             Review(
-                                id=1,
+                                id='review_id_456',
                                 publication_name='the needledrop',
                                 score=70,
                                 date='',
                                 link=''
                             )
-                        ],
-                        date=None,
-                        type=None,
-                        total_tracks=None,
-                        spotify_url=None)
+                        ]
+                    )
                 ]
             )
         }
